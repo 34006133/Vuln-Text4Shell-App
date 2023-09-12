99a95aaab0417bf3a8c7938b3ddd50ddd32e3cf5
htbu <htbu@gmail.com>
2023-09-12 15:58:30 +0800

    Added Spring boilerplate.

---
+++ text4shell/src/test/java/com/vulnapp/text4shell/Text4shellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vulnapp.text4shell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Text4shellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
