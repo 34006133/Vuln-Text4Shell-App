e476c81cf240db168c317315761aa88ad457e80b
htbu <htbu@gmail.com>
2023-09-13 13:41:54 +0800

    Got index.html to show, imported org.apache.commons.text for vuln

--- text4shell/src/main/java/com/vulnapp/text4shell/VulnApplication.java
+++ text4shell/src/main/java/com/vulnapp/text4shell/VulnApplication.java
@@ -1,10 +1,17 @@
 package com.vulnapp.text4shell;
 
+import org.apache.commons.text.StringSubstitutor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @RestController
@@ -12,8 +19,14 @@ public class VulnApplication {
     public static void main(String[] args) {
       SpringApplication.run(VulnApplication.class, args);
     }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public ModelAndView index() {
+              ModelAndView modelAndView = new ModelAndView();
+              modelAndView.setViewName("index.html");
+              return modelAndView;
     }
+
 }
+
